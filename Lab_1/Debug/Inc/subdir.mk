1ff3901b60563148c119aa98a60af54c90f6a4c1
mkupriyanov05 <114899758+mkupriyanov05@users.noreply.github.com>
2025-10-19 16:11:35 +0300

    Raw version after lab execution at IU4 workplace

---
+++ Lab_1/Debug/Inc/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Inc/stm32f10x.s 
+
+OBJS += \
+./Inc/stm32f10x.o 
+
+S_DEPS += \
+./Inc/stm32f10x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Inc/%.o: ../Inc/%.s Inc/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -DDEBUG -c -I"C:/Users/user/Desktop/leo/Lab_1/Inc" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
+clean: clean-Inc
+
+clean-Inc:
+	-$(RM) ./Inc/stm32f10x.d ./Inc/stm32f10x.o
+
+.PHONY: clean-Inc
+
